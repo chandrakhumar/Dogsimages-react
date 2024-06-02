624f75fbb4bc8f77192bba95589dcb9875a07f6f
chandrakhumar <civil.chandra26@gmail.com>
2024-06-02 17:08:13 +0530

    update project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import ReactDOM from "react-dom/client";
+import "./style.css"
+import One from "./assets/images/Dog-Picture6.jpg"
+import Two from "./assets/images/364377-puppies-puppy-baby-dog-dogs-41.jpg"
+import Three from "./assets/images/animal-blur-canine-551628.jpg"
+import Four from "./assets/images/wp9015508.webp"
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+
+function Dogs(props){
+  return(
+   
+      <div class="dogNames">
+      
+            <img src= {props.dogPic} alt="dog1" ></img>
+            <div>{props.dogName}</div>
+        
+    </div>
+ 
+  )
+}
+
+var dogList = [
+
+       {
+        dogPic: One,
+        dogName: "Dogs are cute"
+       },
+
+
+       {
+        dogPic: Two,
+        dogName: "Dogs are cute"
+       },
+
+       {
+        dogPic:Three,
+        dogName: "Dogs are cute"
+       },
+
+       {
+        dogPic:Four,
+        dogName: "Dogs are cute"
+       },
+       {
+        dogPic: Three,
+        dogName: "Dogs are cute"
+       },
+
+
+       {
+        dogPic: Four,
+        dogName: "Dogs are cute"
+       },
+
+       {
+        dogPic: One,
+        dogName: "Dogs are cute"
+       },
+
+       {
+        dogPic:Two,
+        dogName: "Dogs are cute"
+       }
+       
+       
+
+      ]
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div class="container">
+    {
+      dogList.map(function(item,index){
+
+        return <Dogs key={index} dogPic={item.dogPic} dogName={item.dogName}></Dogs>
+
+
+
+      })
+    }
+  </div>
+  
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
